1066956398e87b3e3fd9b63274939315a0c4f1ee
LordFeexz <anandafiqri1403@gmail.com>
2023-01-06 08:41:30 +0700

    login

--- controllers/index.go
+++ controllers/index.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 
+	h "github.com/LordFeexz/Nusa-Tech/helpers"
 	m "github.com/LordFeexz/Nusa-Tech/models"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
@@ -39,21 +40,48 @@ func ReadDataById(c *gin.Context) {
 func Register(c *gin.Context) {
 	var user m.User
 
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
-		return
+	email, password := c.PostForm("email"), c.PostForm("password")
+
+	if email == "" || password == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid input"})
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
 	}
 
+	user.Email = email
+
 	user.Password = string(hashedPassword)
 
 	m.Db.Create(&user)
 
 	c.JSON(http.StatusCreated, gin.H{"message": user})
+}
+
+func Login(c *gin.Context) {
+	var user m.User
+
+	email, password := c.PostForm("email"), c.PostForm("password")
+
+	m.Db.Where("email = ?", email).First(&user)
+
+	if user.Id == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid email/password"})
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid email/password"})
+		return
+	}
+
+	access_token, _ := h.CreateToken(user)
+
+	c.JSON(http.StatusOK, gin.H{"access_token": access_token})
 }
